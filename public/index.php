91e38d8edc7d885ff489c65efee29795f7c99694
Johan Groenen <johan.groenen+github@gmail.com>
2017-09-14 21:12:00 +0200

    mail

--- public/index.php
+++ public/index.php
@@ -3,6 +3,13 @@
     $uri_parts = array_values(array_filter(explode("/", explode("?", $uri)[0])));
     $cbase = $uri_parts[0];
     
+    if (!empty($_POST["cbase_name"]) && !empty($_POST["admin_email"])) {
+        // generate token
+        // register cbase with admin e-mail and check token does not exist (store tokens blowfish)
+        mail($_POST["admin_email"], "cbase admin link", "cbase created; here is your admin link");
+        exit('thanks'); // replace with template
+    }
+    
     // check if cbase exists
     
     // if exists, then show search
@@ -29,8 +36,8 @@
         <?php } else { ?>
             <form method="post">
                 <h1>Create a new CBase</h1>
-                CBase name: <input type="text"><br>
-                Admin e-mail: <input type="email"><br>
+                CBase name: <input name="cbase_name" type="text"><br>
+                Admin e-mail: <input name="admin_email" type="email"><br>
                 <button>Create CBase</button>
             </form>
         <?php } ?>
